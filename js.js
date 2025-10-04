9943b73e750fca4c3c3f9488bef98281312a34d3
garzonlj <garzonlj@javeriana.edu.co>
2025-10-03 23:02:02 -0500

    portada

---
+++ js.js
@@ -0,0 +1,13 @@
+let bg = document.getElementById("bg");
+let sol= document.getElementById("sol");
+let gato= document.getElementById("gato");
+let nave= document.getElementById("nave");
+let text= document.getElementById("text")
+window.addEventListener('scroll', function(){
+    var value = this.window.scrollY;
+
+    bg.style.top = value * 0.5 + 'px';
+    sol.style.left= -value * 0.5 + 'px';
+    nave.style.top= value * 0.15 + 'px';
+    text.style.top = value * 1 + 'px';
+})
