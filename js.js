0551513917f148e422042ec999962ef76058ec68
garzonlj <garzonlj@javeriana.edu.co>
2025-10-04 15:05:48 -0500

    qn es

--- js.js
+++ js.js
@@ -2,13 +2,33 @@ let bg = document.getElementById("bg");
 let sol= document.getElementById("sol");
 let gato= document.getElementById("gato");
 let nave= document.getElementById("nave");
-let text= document.getElementById("text")
-window.addEventListener('scroll', function(){
-    var value = this.window.scrollY;
+let text= document.getElementById("text");
 
+let luna = document.getElementById("luna");
+let gatoqn = document.getElementById("gatoqn");
+
+let fichaDerecha = document.querySelector(".ficha-derecha");
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+
+ 
     bg.style.top = value * 0.5 + 'px';
     sol.style.left= -value * 0.5 + 'px';
     nave.style.top= value * 0.15 + 'px';
     text.style.top = value * 1 + 'px';
-    gato.style.left = -value * -0.3 + 'px';
-})
+    gato.style.left = value * 0.3 + 'px'; 
+
+    
+    let start = fichaDerecha.offsetTop; 
+    let end = start + fichaDerecha.offsetHeight;
+
+
+    if(value >= start && value <= end){
+        let relativeScroll = value - start; 
+
+    
+        luna.style.top = relativeScroll * 0.5 + 'px';
+        gatoqn.style.top = -relativeScroll * 0.5 + 'px';
+    }
+});
